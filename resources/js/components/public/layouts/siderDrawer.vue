ecbb65cc928768bd4880bf744d7b8513ad0f1f4b
Santiago Pegels <santiagopegels@gmail.com>
2021-01-20 12:17:10 -0300

    add sessionStorage

--- resources/js/components/public/layouts/siderDrawer.vue
+++ resources/js/components/public/layouts/siderDrawer.vue
@@ -1,26 +1,6 @@
 <template>
     <div>
         <a-menu mode="horizontal" style="text-align: center">
-            <a-menu-item key="order">
-                <a-select size="large" default-value="relevantes" style="width: 100%">
-                    <a-select-option value="relevantes">
-                        Más relevantes
-                    </a-select-option>
-                    <a-select-option value="vendidos">
-                        Más vendidos
-                    </a-select-option>
-                    <a-select-option value="lanzamientos">
-                        Lanzamientos
-                    </a-select-option>
-                    <a-select-option value="bajos">
-                        Precio más bajos
-                    </a-select-option>
-                    <a-select-option value="altos">
-                        Precio más altos
-                    </a-select-option>
-                </a-select>
-            </a-menu-item>
-            <a-divider type="vertical"/>
             <a-menu-item key="filter" @click="toggleSiderFilter()">
                 <a-icon type="control"/>
                 Filtros
--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -6,13 +6,16 @@ import getters from './getters'
 import state from "./state";
 import VuexPersistence from 'vuex-persist';
 
-
 Vue.use(Vuex);
+
+const vuexSession = new VuexPersistence({
+    storage: window.sessionStorage
+})
 
 export default new Vuex.Store({
     state,
     mutations,
     getters,
     actions,
-    plugins: [new VuexPersistence().plugin]
+    plugins: [vuexSession.plugin]
 })
