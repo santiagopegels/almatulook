e36542029b32d5195c37d2eb3b137ee8189a05b9
Santiago Pegels <santiagopegels@gmail.com>
2021-12-01 13:33:57 -0300

    add title into head

--- resources/views/public/layouts/app.blade.php
+++ resources/views/public/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Almatulook</title>
     @include('public.layouts.partials.styles')
 </head>
 
